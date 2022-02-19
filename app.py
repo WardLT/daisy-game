80318c400ab6eabc65512796cdc8bf3825f70f27
Logan Ward <ward.logan.t@gmail.com>
2022-02-19 17:45:27 -0500

    Add a page to display guesses

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from datetime import datetime
+import secrets
 import json
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, flash, session
 from werkzeug.utils import redirect
+import pandas as pd
 
 app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(16)
 
 breeds = [
     "Airedale Terrier",
@@ -27,6 +30,8 @@ breeds = [
     "Rottweiler"
 ]
 
+breed_tags = [x.replace(" ", "_") for x in breeds]
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -39,7 +44,36 @@ def receive():
     data = dict(request.form)
     data['response_time'] = datetime.now().isoformat()
 
+    # Convert breeds to percentages
+    for b in breed_tags:
+        data[b] = float(data.get(b, 0))
+
+    # Store the person's name in the session
+    session['name'] = data['name']
+    session['newbreed'] = data['newbreed']
+
+    # Make sure they guess at least one breed
+    score_count = sum(data.get(x, 0) for x in breed_tags)
+    if score_count <= 0:
+        flash('You must assign a percentage to at least one breed!', 'error')
+        return redirect('/')
+
     with open('results.json', 'a') as fp:
         print(json.dumps(data), file=fp)
 
-    return redirect('/')
+    return redirect('/guesses')
+
+
+@app.route('/guesses')
+def guesses():
+    # Get the most-recent guess from each person
+    results = pd.read_json('results.json', lines=True)
+    results.sort_values(['response_time', 'name'], ascending=True, inplace=True)
+    results.drop_duplicates('name', inplace=True)
+
+    # Compute percentages
+    total = results[breed_tags].sum(axis=1).values[:, None]
+    results[breed_tags] = results[breed_tags] / total * 100
+
+    # Print the form
+    return render_template('guesses.html', results=results.to_dict(orient='records'), breeds=breeds)
